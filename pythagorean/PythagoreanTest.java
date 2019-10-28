210db354b6d8bce30c6c041d817443f1acc58288
capt-alien <eric.botcher@presidio.edu>
2019-10-28 13:37:50 -0700

    added Pythaorgeran test

---
+++ pythagorean/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean result = new Pythagorean();
+        // String currentDate = iD.getCurrentDate();
+        double something = result.calculateHypotenuse(3, 5);
+        System.out.println(something);
+    }
+}

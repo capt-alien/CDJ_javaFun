01727b903c12b963aad123bf1601ac2d46537fbb
capt-alien <eric.botcher@presidio.edu>
2019-10-28 13:17:44 -0700

    first commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main(String[] args){
+        System.out.println("My Name is Capt. Alien");
+        System.out.println("I am 36 years old");
+        System.out.println("My hometown is Wisconsin Rapids, WI");
+    }
+}

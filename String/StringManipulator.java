f29fe010fc72d23b37df2c4a4624e9bc483f12c2
capt-alien <eric.botcher@presidio.edu>
2019-10-28 16:20:56 -0700

    started manip

---
+++ String/StringManipulator.java
@@ -0,0 +1,30 @@
+public class StringManipulator{
+    // Trim and Concat
+    public String trimAndConcat(String str1, String str2){
+        String a = str1.trim();
+        String b = str2.trim();
+        String z = a.concat(b);
+        return z;
+    }
+    // Get Index or Null
+    public Integer getIndexOrNUll(String str, char n){
+        Integer x = str.indexof(n);
+        if(x == -1){
+            return null;
+        } else{
+            return x;
+        }
+    }
+    public Integer getIndexOrNull(String strA, String strB){
+        Integer x = strA.indexof(strB);
+        if(x == -1){
+            return null;
+        } else{
+            return x;
+        }
+    }
+    // Concat Substring
+    public String ConcatSubstring(String, int, int, String){
+
+    }
+}

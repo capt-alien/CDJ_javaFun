bf982b809e9f91134fe3acc3a5137a81374c8cf3
capt-alien <eric.botcher@presidio.edu>
2019-10-29 13:33:05 -0700

    progress on Basics

--- basics/Basics.java
+++ basics/Basics.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Basics{
     // print 1-255
     public static void Print255(){
@@ -21,13 +23,39 @@ public class Basics{
         }
     }
     // ITTERATING THROUGH AN ARRAY
-
-
+    public static void arrayItter(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
     // FIND MAX
+    public static int findMax(int[] arr){
+        int max = arr[0];
+        for (int i=1; i < arr.length; i++){
+            if (arr[i]< max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 
     // GET ABVERAGE
+    public static int findAvg(int[] arr){
+        int total = 0;
+        for (int i = 0; i< arr.length; i++){
+            total += arr[i];
+        }
+        return total/arr.length;
+    }
 
     // Array with Odd Numbers
+    public static ArrayList<Integer> arrOfOdd(){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 1; i <= 255; i= i+2){
+            arr.add(i);
+        }
+        return arr;
+    }
 
     // Greater Than Y
 
--- basics/TestBasics.java
+++ basics/TestBasics.java
@@ -10,8 +10,27 @@ public class TestBasics{
         //should print odd to 255
 
         // testbot.PrintOdd255();
-        
+
         //Test PRint sum()
-        testbot.printsum();
+        // testbot.printsum();
+
+        // TEST arrayItter
+        // int[] array = {1,3,5,7,9,13};
+        // testbot.arrayItter(array);
+
+        // TEST FIND MAX
+        // int[] arr = {-3, -5, -7};
+        // System.out.println(testbot.findMax(arr));
+
+        // TEST GET Average
+        // int[] avgArr= {2,10,3};
+        // System.out.println(testbot.findAvg(avgArr));
+
+        // TEST ARR WITH ODD Numbers
+        System.out.println(testbot.arrOfOdd());
+
+
+
+        // END
     }
 }
